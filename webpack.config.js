d464728bd008a3a1d2b56c088d7711c5ffd72a5d
jesusalc <jesusalc@gmail.com>
2017-01-02 21:36:34 +0100

    added webpakc and app js

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const webpack = require('webpack')
+const NODE_ENV = process.env.NODE_ENV
+const IS_PROD = NODE_ENV === 'production'
+
+const config = {
+  performance: {
+    hints: IS_PROD
+  },
+  // devtool: IS_PROD ? 'source-map' : 'cheap-eval-source-map',
+  devtool: 'source-map',
+  entry: './src',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js',
+    library: 'App',
+  },
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      loader: 'babel-loader',
+      exclude: 'node_modules',
+      options: {
+        presets: ['react']
+      }
+    }]
+  }
+}
+
+
+if (IS_PROD) {
+  const BabiliPlugin = require('babili-webpack-plugin')
+  config.plugins.push(
+    new BabiliPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    })
+  )
+}
+
+
+module.exports = config
